d8f0545bb4a6615e4882332a5ed1369d56aa656a
Sylvia Soto <sylviadiane69@gmail.com>
2020-04-09 08:24:26 -0500

    changes to the server.js file

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,51 +1,67 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-
-//var express = require("express");
-//var path = require("path");
-
-// Sets up the Express App
-// =============================================================
-//var app = express();
-//var PORT = 3000;
+//Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+let data = JSON.parse(fs.readFileSync("./json/db.json", "utf8"));
 
 // Sets up the Express app to handle data parsing
-//app.use(express.urlencoded({ extended: true }));
-//app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // The routes that get the html
-//app.get("/notes", function (req, res) {
-//res.sendFile(path.join(__dirname, "notes.html"));
-//});
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
 
-//app.get("*", function (req, res) {
-//res.sendFile(path.join(__dirname, "index.html"));
-//});
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-// Displays all characters
-//app.get("/api/notes", function (req, res) {
-//return res.json(notes);
-//});
+// Displays all notes
+app.get("/api/notes", function (req, res) {
+  return res.json(notes);
+});
 
 // Create New notes - takes in JSON input
-//app.post("/api/notes", function (req, res) {
-// req.body hosts is equal to the JSON post sent from the user
-// This works because of our body parsing middleware
-//var newNotes = req.body;
+app.post("/api/notes", function (req, res) {
+  const newNotes = req.body;
 
-// Using a RegEx Pattern to remove spaces from newNotes
-// You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//newNotes.routeName = newNotes.name.replace(/\s+/g, "").toLowerCase();
+  data.push(newNotes);
 
-//console.log(newNotes);
-
-//notes.push(newNotes);
-
-//res.json(newNotes);
-//});
-
-
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
 });
+
+app.delete("/api/notes/:id"), function (req, res) {
+  let id = req.params.id;
+  data = data.filter(function (note) {
+    if (note.id === id) {
+      return false;
+    }
+    return true;
+  });
+
+  fs.writeFile("./json/db.json", JSON.stringify(data), function (err) {
+    if (err) throw err;
+    console.log("test");
+
+  });
+
+  return res.json(true);
+
+  fs.writeFile("./json/db.json", JSON.stringify(data), function (err) {
+    if (err) throw err;
+    res.end();
+  });
+
+  return res.json(data);
+
+  // Starts the server to begin listening
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+
+};
+
